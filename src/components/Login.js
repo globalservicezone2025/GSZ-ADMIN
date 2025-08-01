7a74a9a19d02ef60159cd03b30e72678bf7c6f36
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2025-08-01 23:14:02 +0600

    fix: Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,10 +9,9 @@ import "./css/login.css";
 const Login = () => {
   const router = useHistory();
   const [email, setEmail] = useState("");
-  const [otp, setOtp] = useState("");
+  const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
-  const [otpError, setOtpError] = useState("");
-  const [showOtp, setShowOtp] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const Error = (text) =>
@@ -62,103 +61,68 @@ const Login = () => {
   //   initializeApi();
   // }, []);
 
-  useEffect(() => {
-    const init = async () => {
-      if (showOtp) {
-        // setTimeout(async () => {
-        await fetchData(`/auth/login-with-otp`, "POST", {
-          // roleId: "roleId",
-          email,
-          otp: "1234",
-        });
-        // }, 1000);
-      }
-    };
+  // useEffect(() => {
+  //   const init = async () => {
+  //     if (showOtp) {
+  //       // setTimeout(async () => {
+  //       await fetchData(`/auth/login-with-otp`, "POST", {
+  //         // roleId: "roleId",
+  //         email,
+  //         otp: "1234",
+  //       });
+  //       // }, 1000);
+  //     }
+  //   };
 
-    init();
-  }, [showOtp]);
+  //   init();
+  // }, [showOtp]);
 
-  const handelSubmitEmail = async (email) => {
+  const handleSubmitLogin = async (email, password) => {
     setLoading(true);
 
+    let hasError = false;
     if (!isValidEmail(email)) {
       setEmailError("Email is invalid!");
-      return;
+      hasError = true;
     } else {
       setEmailError("");
-      // await fetchData("/api/v1/auth/send-login-otp", "POST", {
-      //   email,
-      // });
-
-      const jsonData = await fetchData("/api/v1/auth/send-login-otp", "POST", {
-        email,
-        type: "admin",
-      });
-
-      const message = jsonData.message;
-
-      if (!jsonData.success) {
-        // eslint-disable-next-line no-throw-literal
-        setLoading(false);
-        showErrorToast(jsonData?.message);
-        throw {
-          message,
-        };
-      }
-
-      setOtp("");
-      setShowOtp(true);
-      showSuccessToast(jsonData.message);
-      setLoading(false);
-
-      // setTimeout(async () => {
-      //   await fetchData(`/auth/login-with-otp`, "POST", {
-      //     // roleId: "roleId",
-      //     email,
-      //     otp,
-      //   });
-      // }, 1000);
-
-      return { success: true, message: message };
     }
-  };
-
-  const handelSubmitOtp = async (email, otp) => {
-    setLoading(true);
-
-    if (otp.length === 0) {
-      setOtpError("Otp is empty!");
-      return;
+    if (!password || password.length < 4) {
+      setPasswordError("Password is required!");
+      hasError = true;
     } else {
-      setOtpError("");
-      const jsonData = await fetchData("/api/v1/auth/login-with-otp", "POST", {
-        email,
-        otp,
-      });
-
-      const message = jsonData.message;
-      if (!jsonData.success) {
-        setLoading(false);
-        showErrorToast(jsonData?.message);
-        // eslint-disable-next-line no-throw-literal
-        throw {
-          message,
-        };
-      }
-      createLogin(
-        jsonData.data.id,
-        jsonData.data.roleId,
-        jsonData.data.email,
-        jsonData.data.accessToken
-      );
-
-      showSuccessToast(jsonData.message);
-      // router.push("/dashboard");
-      window.location.href = "/dashboard";
-      setLoading(false);
-
-      return { success: true, message: message };
+      setPasswordError("");
     }
+    if (hasError) {
+      setLoading(false);
+      return;
+    }
+
+    const jsonData = await fetchData("/api/v1/auth/login", "POST", {
+      email,
+      password,
+    });
+
+    const message = jsonData.message;
+    if (!jsonData.success) {
+      setLoading(false);
+      showErrorToast(jsonData?.message);
+      throw {
+        message,
+      };
+    }
+    createLogin(
+      jsonData.data.id,
+      jsonData.data.roleId,
+      jsonData.data.email,
+      jsonData.data.accessToken
+    );
+
+    showSuccessToast(jsonData.message);
+    window.location.href = "/dashboard";
+    setLoading(false);
+
+    return { success: true, message: message };
   };
 
   return (
@@ -174,108 +138,58 @@ const Login = () => {
                       <h3 className="text-center mb-4 text-white">
                         Global Service Zone Dashboard
                       </h3>
-                      <form className="mt-4">
-                        {showOtp ? (
-                          <>
-                            <div className="form-group">
-                              <label htmlFor="otp" className="mb-1 text-white">
-                                <strong>OTP</strong>
-                              </label>
-                              <input
-                                type="text"
-                                id="otp"
-                                name="otp"
-                                className="form-control"
-                                value={otp}
-                                onChange={(e) =>
-                                  setOtp(parseInt(e.target.value))
-                                }
-                              />
-                              {Error(otpError)}
-                              <p className="text-white mt-2">
-                                Did not get otp?{" "}
-                                {loading ? (
-                                  <span style={{ cursor: "pointer" }}>
-                                    Sending...
-                                  </span>
-                                ) : (
-                                  <span
-                                    style={{ cursor: "pointer" }}
-                                    onClick={() => handelSubmitEmail(email)}
-                                  >
-                                    Send again
-                                  </span>
-                                )}
-                              </p>
-                            </div>
-                            <div className="text-center login_btn_div">
-                              {loading ? (
-                                <>
-                                  <button
-                                    type="button"
-                                    className="btn bg-white text-primary btn-block"
-                                    // onClick={() => handelSubmitOtp()}
-                                  >
-                                    Loading...
-                                  </button>
-                                </>
-                              ) : (
-                                <>
-                                  <button
-                                    type="button"
-                                    className="btn bg-white text-primary btn-block"
-                                    onClick={() => handelSubmitOtp(email, otp)}
-                                  >
-                                    Login
-                                  </button>
-                                </>
-                              )}
-                            </div>
-                          </>
-                        ) : (
-                          <>
-                            <div className="form-group">
-                              <label
-                                htmlFor="email"
-                                className="mb-1 text-white"
-                              >
-                                <strong>Email</strong>
-                              </label>
-                              <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                className="form-control"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                              />
-                              {Error(emailError)}
-                            </div>
-                            <div className="text-center login_btn_div">
-                              {loading ? (
-                                <>
-                                  <button
-                                    type="button"
-                                    className="btn bg-white text-primary btn-block"
-                                    // onClick={() => handelSubmitEmail()}
-                                  >
-                                    Loading...
-                                  </button>
-                                </>
-                              ) : (
-                                <>
-                                  <button
-                                    type="button"
-                                    className="btn bg-white text-primary btn-block"
-                                    onClick={() => handelSubmitEmail(email)}
-                                  >
-                                    Send OTP
-                                  </button>
-                                </>
-                              )}
-                            </div>
-                          </>
-                        )}
+                      <form
+                        className="mt-4"
+                        onSubmit={(e) => {
+                          e.preventDefault();
+                          handleSubmitLogin(email, password);
+                        }}
+                      >
+                        <div className="form-group">
+                          <label htmlFor="email" className="mb-1 text-white">
+                            <strong>Email</strong>
+                          </label>
+                          <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="form-control"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                          {Error(emailError)}
+                        </div>
+                        <div className="form-group">
+                          <label htmlFor="password" className="mb-1 text-white">
+                            <strong>Password</strong>
+                          </label>
+                          <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="form-control"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                          {Error(passwordError)}
+                        </div>
+                        <div className="text-center login_btn_div">
+                          {loading ? (
+                            <button
+                              type="button"
+                              className="btn bg-white text-primary btn-block"
+                            >
+                              Loading...
+                            </button>
+                          ) : (
+                            <button
+                              type="submit"
+                              className="btn bg-white text-primary btn-block"
+                            >
+                              Login
+                            </button>
+                          )}
+                        </div>
                       </form>
                     </div>
                   </div>
